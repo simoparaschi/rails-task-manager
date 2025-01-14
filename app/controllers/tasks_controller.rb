f9a579590b79cfdd42112c3485589b385acaf6e0
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-14 15:16:41 +0100

    Added option to add new taks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,20 @@ class TasksController < ApplicationController
     @id = params[:id]
     @current_task = Task.find(@id)
   end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @task = Task.new(form_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def form_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/tasks", to: "tasks#list"
+  get "/tasks/new", to: "tasks#new", as: :new
+  post "/tasks", to: "tasks#create"
   get "/tasks/:id", to: "tasks#find", as: :task
 end
